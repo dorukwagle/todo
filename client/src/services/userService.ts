5998da5dbbf2c8e909c09117c85084b4157b1eda
dorukwagle <dorukwagle@gmail.com>
2024-12-28 12:43:25 +0545

    fix typo in services

--- client/src/services/userService.ts
+++ client/src/services/userService.ts
@@ -1,6 +1,6 @@
 import APIClient from "./apiClient";
 import User from "../entities/User";
 
-const userService = new APIClient<User, User>("/user");
+const userService = new APIClient<User, User>("/users");
 
 export default userService;
